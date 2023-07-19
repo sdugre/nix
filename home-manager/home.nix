9520c2ddf33dfed0132aaf692add3934a682065a
sdugre <sdugre@gmail.com>
2023-07-19 10:22:34 -0400

    add NUR

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -9,6 +9,7 @@
 
     # Or modules exported from other flakes (such as nix-colors):
     # inputs.nix-colors.homeManagerModules.default
+    inputs.nur.hmModules.nur
 
     # You can also split up your configuration and import pieces of it here:
     ./config/gnome.nix
