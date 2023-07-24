5e606eecb61e3bc4f9f38e415b4033497adce74c
sdugre <sdugre@gmail.com>
2023-07-23 21:42:43 -0400

    add filesystem option to show network drives

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -182,21 +182,27 @@
   fileSystems."/mnt/video" = {
     device = "192.168.1.16:/volume1/video";
     fsType = "nfs";
+    options = ["x-gvfs-show"];
   };
   fileSystems."/mnt/music" = {
     device = "192.168.1.16:/volume1/music";
     fsType = "nfs";
+    options = ["x-gvfs-show"];
   }; 
   fileSystems."/mnt/photo" = {
     device = "192.168.1.16:/volume1/photo";
     fsType = "nfs";
+    options = ["x-gvfs-show"];
   }; 
   fileSystems."/mnt/downloads" = {
     device = "192.168.1.16:/volume1/downloads";
     fsType = "nfs";
-  }; 
+    options = ["x-gvfs-show"];
+  };
+
   fileSystems."/mnt/homes" = {
     device = "192.168.1.16:/volume1/homes";
     fsType = "nfs";
+    options = ["x-gvfs-show"];
   }; 
 }
