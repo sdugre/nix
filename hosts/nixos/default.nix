01f22f316387e847d2d38e7f7d1448cd73394de6
Sean Dugre <sdugre@gmail.com>
2023-08-06 22:06:12 -0400

    refactor tailscale

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -153,8 +153,6 @@
     passwordAuthentication = true;
   };
 
-  services.tailscale.enable = true;
-
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.05";
 
