db6562696e95342e362599e0bccb39adad2c306e
Sean Dugre <sdugre@gmail.com>
2023-08-07 20:18:55 -0400

    refactor

--- hosts/thinkpad/default.nix
+++ hosts/thinkpad/default.nix
@@ -8,37 +8,6 @@
     ./config/gnome.nix
   ];
 
-  nixpkgs = {
-    # You can add overlays here
-    overlays = [
-      # Add overlays your own flake exports (from overlays and pkgs dir):
-      outputs.overlays.additions
-      outputs.overlays.modifications
-      outputs.overlays.unstable-packages
-
-      # You can also add overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
-    ];
-    # Configure your nixpkgs instance
-    config = {
-      # Disable if you don't want unfree packages
-      allowUnfree = true;
-      packageOverrides = pkgs: {
-        nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
-          inherit pkgs;
-        };
-      };
-    };
-  };
-
-  # FIXME: Add the rest of your current configuration
   # Enable networking
   networking.networkmanager.enable = true;
 
