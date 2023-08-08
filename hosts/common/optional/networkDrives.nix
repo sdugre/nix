f4abe8cadb2e0be8b5f7c92921dcfcc4f776f319
Sean Dugre <sdugre@gmail.com>
2023-08-07 22:36:35 -0400

    change network drive mounts to use tailscale

--- hosts/common/optional/networkDrives.nix
+++ hosts/common/optional/networkDrives.nix
@@ -1,5 +1,6 @@
 {
 
+  # mount NAS drives via tailscale
   fileSystems."/mnt/video" = {
     device = "100.84.193.57:/volume1/video";
     fsType = "nfs";
@@ -11,22 +12,22 @@
     options = ["x-gvfs-show"];
   }; 
   fileSystems."/mnt/photo" = {
-    device = "192.168.1.16:/volume1/photo";
+    device = "100.84.193.57:/volume1/photo";
     fsType = "nfs";
     options = ["x-gvfs-show"];
   }; 
   fileSystems."/mnt/downloads" = {
-    device = "192.168.1.16:/volume1/downloads";
+    device = "100.84.193.57:/volume1/downloads";
     fsType = "nfs";
     options = ["x-gvfs-show"];
   };
   fileSystems."/mnt/homes" = {
-    device = "192.168.1.16:/volume1/homes";
+    device = "100.84.193.57:/volume1/homes";
     fsType = "nfs";
     options = ["x-gvfs-show"];
   }; 
   fileSystems."/mnt/docs" = {
-    device = "192.168.1.16:/volume1/docs";
+    device = "100.84.193.57:/volume1/docs";
     fsType = "nfs";
     options = ["x-gvfs-show"];
   }; 
