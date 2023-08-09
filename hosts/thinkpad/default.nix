b14f231a029b12ba7cf2a0d409625839784bf810
Sean Dugre <sdugre@gmail.com>
2023-08-08 22:16:18 -0400

    refactor

--- hosts/thinkpad/default.nix
+++ hosts/thinkpad/default.nix
@@ -7,6 +7,7 @@
 
     ../common/optional/networkDrives.nix
     ../common/optional/gnome.nix
+    ../common/optional/pipewire.nix
   ];
 
   # Enable networking
@@ -28,25 +29,6 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-
 
   # TODO: Set your hostname
   networking.hostName = "thinkpad";
