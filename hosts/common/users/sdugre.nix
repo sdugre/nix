6c8f7e6ee50c1eb711a95dfb8d2e62a5812afe08
Sean Dugre <sdugre@gmail.com>
2023-08-08 23:02:03 -0400

    refactor host user

---
+++ hosts/common/users/sdugre.nix
@@ -0,0 +1,15 @@
+{
+  users.users = {
+    sdugre = {
+      # initialPassword = "correcthorsebatterystaple";
+      isNormalUser = true;
+      description = "Sean Dugre";
+      shell = pkgs.zsh;
+      openssh.authorizedKeys.keys = [
+        # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
+      ];
+      # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
+      extraGroups = [ "networkmanager" "wheel" ];
+    };
+  };
+}
--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -5,6 +5,7 @@
   imports = [
     ./hardware-configuration.nix
     ../common/global
+    ../common/users/sdugre
 
     ../common/optional/networkDrives.nix
     ../common/optional/gnome.nix
@@ -40,27 +41,6 @@
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
   boot.loader.grub.configurationLimit = 10;
-
-  programs.zsh.enable = true;
-
-  # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
-  users.users = {
-    # FIXME: Replace with your username
-    sdugre = {
-      # TODO: You can set an initial password for your user.
-      # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
-      # Be sure to change it (using passwd) after rebooting!
-      # initialPassword = "correcthorsebatterystaple";
-      isNormalUser = true;
-      description = "Sean Dugre";
-      shell = pkgs.zsh;
-      openssh.authorizedKeys.keys = [
-        # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
-      ];
-      # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
-      extraGroups = [ "networkmanager" "wheel" ];
-    };
-  };
 
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
