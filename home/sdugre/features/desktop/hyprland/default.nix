04944f74ea72fce94ac31717f8b9bbeec3fa918f
Sean Dugre <sdugre@gmail.com>
2023-08-26 22:45:28 -0400

    hyprland first try

--- home/sdugre/features/desktop/hyprland/default.nix
+++ home/sdugre/features/desktop/hyprland/default.nix
@@ -10,13 +10,5 @@
   wayland.windowManager.hyprland = {
     enable = true;
     xwayland.enable = true;
-    settings = {
-      general = {
-        gaps_in = 15;
-        gaps_out = 20;
-        border_size = 2.7;
-        cursor_inactive_timeout = 4;
-      };
-    };
   };
 }
