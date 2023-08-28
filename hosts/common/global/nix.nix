adeb397ebd6c7cf7e24d5e91064b2d9ac5c21a11
Sean Dugre <sdugre@gmail.com>
2023-08-27 21:30:29 -0400

    enable cachix for hyprland

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -13,6 +13,8 @@
       experimental-features = "nix-command flakes";
       auto-optimise-store = true;
       warn-dirty = false;
+      substituters = ["https://hyprland.cachix.org"];
+      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
     };
   };
 
