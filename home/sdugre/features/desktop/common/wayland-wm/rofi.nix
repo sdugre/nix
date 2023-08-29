6cfeed90a1cfae19ecf54c3535c4dde14fcdbd5f
Sean Dugre <sdugre@gmail.com>
2023-08-28 22:56:49 -0400

    initial rofi config

--- home/sdugre/features/desktop/common/wayland-wm/rofi.nix
+++ home/sdugre/features/desktop/common/wayland-wm/rofi.nix
@@ -2,5 +2,13 @@
 {
   programs.rofi = {
     enable = true;
+    package = pkgs.rofi-wayland;
+    extraConfig = {
+      modi = "run,drun,window";
+      show-icons = true;
+      drun-display-format = "{icon} {name}";
+      location = 0;
+      disable-history = false;
+      hide-scrollbar = false;
   };
 }
