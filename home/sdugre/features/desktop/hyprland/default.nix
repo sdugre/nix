99a3b2648bb686df87e972e266ff062e9f6d2219
Sean Dugre <sdugre@gmail.com>
2023-08-29 21:36:39 -0400

    hyprland

--- home/sdugre/features/desktop/hyprland/default.nix
+++ home/sdugre/features/desktop/hyprland/default.nix
@@ -3,6 +3,7 @@
     ../common
     ../common/wayland-wm
     inputs.hyprland.homeManagerModules.default
+    ./config.nix
   ];
 
   home.packages = with pkgs; [
@@ -10,6 +11,61 @@
 
   wayland.windowManager.hyprland = {
     xwayland.enable = true;
+
+    settings = {
+      general = {
+        gaps_in = 15;
+        gaps_out = 20;
+        border_size = 2.7;
+        cursor_inactive_timeout = 4;
+      };
+      input = {
+        kb_layout = "br,us";
+        touchpad.disable_while_typing = false;
+      };
+      dwindle.split_width_multiplier = 1.35;
+      misc.vfr = "on";
+
+      decoration = {
+        active_opacity = 0.94;
+        inactive_opacity = 0.84;
+        fullscreen_opacity = 1.0;
+        rounding = 5;
+        blur = {
+          enabled = true;
+          size = 5;
+          passes = 3;
+          new_optimizations = true;
+          ignore_opacity = true;
+        };
+        drop_shadow = true;
+        shadow_range = 12;
+        shadow_offset = "3 3";
+        "col.shadow" = "0x44000000";
+        "col.shadow_inactive" = "0x66000000";
+      };
+      animations = {
+        enabled = true;
+        bezier = [
+          "easein,0.11, 0, 0.5, 0"
+          "easeout,0.5, 1, 0.89, 1"
+          "easeinback,0.36, 0, 0.66, -0.56"
+          "easeoutback,0.34, 1.56, 0.64, 1"
+        ];
+
+        animation = [
+          "windowsIn,1,3,easeoutback,slide"
+          "windowsOut,1,3,easeinback,slide"
+          "windowsMove,1,3,easeoutback"
+          "workspaces,1,2,easeoutback,slide"
+          "fadeIn,1,3,easeout"
+          "fadeOut,1,3,easein"
+          "fadeSwitch,1,3,easeout"
+          "fadeShadow,1,3,easeout"
+          "fadeDim,1,3,easeout"
+          "border,1,3,easeout"
+        ];
+      };
     extraConfig = import ./config.nix {
 
     };
