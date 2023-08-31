1aea5b22d96b9203af6b544fd54758098290707a
Sean Dugre <sdugre@gmail.com>
2023-08-30 23:31:56 -0400

    hyprland audio

--- home/sdugre/features/desktop/hyprland/config.nix
+++ home/sdugre/features/desktop/hyprland/config.nix
@@ -1,6 +1,8 @@
 {
   home,
-}: let
+  pkgs,
+}: let   
+  pactl = "${pkgs.pulseaudio}/bin/pactl";
   inherit (home.sessionVariables) TERMINAL BROWSER;
 in ''
   # ASCII Art from https://fsymbols.com/generators/carty/
@@ -31,7 +33,6 @@ in ''
     col.shadow_inactive = 0x66000000
   }
   $notifycmd = notify-send -h string:x-canonical-private-synchronous:hypr-cfg -u low
-
   # █▀ █░█ █▀█ █▀█ ▀█▀ █▀▀ █░█ ▀█▀ █▀
   # ▄█ █▀█ █▄█ █▀▄ ░█░ █▄▄ █▄█ ░█░ ▄█
   bind = SUPER, Return, exec, ${TERMINAL}
@@ -39,11 +40,36 @@ in ''
   bind = SUPER, Space, exec, rofi -show drun -modi drun
 
   # Basic Binds
-  bind = SUPERSHIFT, q, killactive
-  bind = SUPERSHIFT, e, exit
+  bind = SUPER, q, killactive
+  bind = SUPERSHIFT, e, exit	
   bind = SUPER, s, togglesplit
   bind = SUPER, f, fullscreen, 1
   bind = SUPERSHIFT, f, fullscreen, 0
   bind = SUPERSHIFT, Space, togglefloating
+  # Mouse Binds
+  bindm=SUPER, mouse:272, movewindow
+  bindm=SUPER, mouse:273, resizewindow
+  # Switch workspaces with mainMod + [0-9]
+  bind = SUPER, 1, workspace, 1
+  bind = SUPER, 2, workspace, 2
+  bind = SUPER, 3, workspace, 3
+  bind = SUPER, 4, workspace, 4
+ # Move active window to a workspace with mainMod + SHIFT + [0-9]
+  bind = SUPER SHIFT, 1, movetoworkspace, 1
+  bind = SUPER SHIFT, 2, movetoworkspace, 2
+  bind = SUPER SHIFT, 3, movetoworkspace, 3
+  bind = SUPER SHIFT, 4, movetoworkspace, 4
+  # Scroll through existing workspaces with mainMod + scroll
+  bind = SUPER, mouse_down, workspace, e+1
+  bind = SUPER, mouse_up, workspace, e-1
+  # Brightness control (only works if the system has lightd)
+  ,XF86MonBrightnessUp,exec,light -A 10
+  ,XF86MonBrightnessDown,exec,light -U 10"
+  # Volume
+  ,XF86AudioRaiseVolume,exec,${pactl} set-sink-volume @DEFAULT_SINK@ +5%
+  ,XF86AudioLowerVolume,exec,${pactl} set-sink-volume @DEFAULT_SINK@ -5%
+  ,XF86AudioMute,exec,${pactl} set-sink-mute @DEFAULT_SINK@ toggle
+  SHIFT,XF86AudioMute,exec,${pactl} set-source-mute @DEFAULT_SOURCE@ toggle
+  ,XF86AudioMicMute,exec,${pactl} set-source-mute @DEFAULT_SOURCE@ toggle
 
 ''
