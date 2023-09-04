ec711349b82d0f0f22abc0015680ffeca2ddb558
Sean Dugre <sdugre@gmail.com>
2023-09-04 15:42:49 -0400

    test waybar config

--- home/sdugre/features/desktop/common/wayland-wm/waybar.nix
+++ home/sdugre/features/desktop/common/wayland-wm/waybar.nix
@@ -5,5 +5,56 @@
     package = pkgs.waybar.overrideAttrs (oa: {
       mesonFlags = (oa.mesonFlags or  [ ]) ++ [ "-Dexperimental=true" ];
     });
+    systemd.enable = true;
+    settings = {
+    
+      primary = {
+        mode = "dock";
+        layer = "top";
+        height = 40;
+        margin = "6";
+        position = "top";
+ 
+        modules-left = [
+          "network"
+          "battery"
+          "tray"
+        ];
+ 
+        modueles-center = [
+          "clock"
+        ];
+
+        battery = {
+          bat = "BAT0";
+          interval = 10;
+          format-icons = [ "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹" ];
+          format = "{icon} {capacity}%";
+          format-charging = "󰂄 {capacity}%";
+          onclick = "";
+        };
+
+
+        clock = {
+          format = "{:%d/%m %H:%M}";
+          tooltip-format = ''
+            <big>{:%Y %B}</big>
+            <tt><small>{calendar}</small></tt>'';
+        };
+
+        network = {
+          interval = 3;
+          format-wifi = "   {essid}";
+          format-ethernet = "󰈁 Connected";
+          format-disconnected = "";
+          tooltip-format = ''
+            {ifname}
+            {ipaddr}/{cidr}
+            Up: {bandwidthUpBits}
+            Down: {bandwidthDownBits}'';
+          on-click = "";
+        };
+      };
+    };
   };	
 }
