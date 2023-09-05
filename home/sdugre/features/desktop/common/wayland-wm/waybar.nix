e7d1d004c311c1fe3ff09d38c4ebfc16d445ef07
Sean Dugre <sdugre@gmail.com>
2023-09-04 22:23:19 -0400

    waybar

--- home/sdugre/features/desktop/common/wayland-wm/waybar.nix
+++ home/sdugre/features/desktop/common/wayland-wm/waybar.nix
@@ -94,5 +94,84 @@ in
         };
       };
     };
+
+    # Cheatsheet:
+    # x -> all sides
+    # x y -> vertical, horizontal
+    # x y z -> top, horizontal, bottom
+    # w x y z -> top, right, bottom, left
+    style = let inherit (config.colorscheme) colors; in /* css */ ''
+      * {
+        font-family: ${config.fontProfiles.regular.family}, ${config.fontProfiles.monospace.family};
+        font-size: 12pt;
+        padding: 0 8px;
+      }
+
+      .modules-right {
+        margin-right: -15px;
+      }
+
+      .modules-left {
+        margin-left: -15px;
+      }
+
+      window#waybar.top {
+        opacity: 0.95;
+        padding: 0;
+        background-color: #${colors.base00};
+        border: 2px solid #${colors.base0C};
+        border-radius: 10px;
+      }
+      window#waybar.bottom {
+        opacity: 0.90;
+        background-color: #${colors.base00};
+        border: 2px solid #${colors.base0C};
+        border-radius: 10px;
+      }
+
+      window#waybar {
+        color: #${colors.base05};
+      }
+
+      #workspaces button {
+        background-color: #${colors.base01};
+        color: #${colors.base05};
+        margin: 4px;
+      }
+      #workspaces button.hidden {
+        background-color: #${colors.base00};
+        color: #${colors.base04};
+      }
+      #workspaces button.focused,
+      #workspaces button.active {
+        background-color: #${colors.base0A};
+        color: #${colors.base00};
+      }
+
+      #clock {
+        background-color: #${colors.base0C};
+        color: #${colors.base00};
+        padding-left: 15px;
+        padding-right: 15px;
+        margin-top: 0;
+        margin-bottom: 0;
+        border-radius: 10px;
+      }
+
+      #custom-hostname {
+        background-color: #${colors.base0C};
+        color: #${colors.base00};
+        padding-left: 15px;
+        padding-right: 18px;
+        margin-right: 0;
+        margin-top: 0;
+        margin-bottom: 0;
+        border-radius: 10px;
+      }
+      #tray {
+        color: #${colors.base05};
+      }
+    '';
+
   };	
 }
