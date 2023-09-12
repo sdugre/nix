566cb2dbdb179e06618e016414598b6f9a9aa03f
Sean Dugre <sdugre@gmail.com>
2023-09-11 22:55:05 -0400

    add backlight script

---
+++ pkgs/backlight/backlight.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+MAX=24242
+MIN=1000
+set -e
+file="/sys/class/backlight/intel_backlight/brightness"
+current=$(cat "$file")
+new="$current"
+if [ "$2" != "" ]; then
+    val=$(echo "$2*$MAX/100" | bc)
+fi
+if [ "$1" = "-inc" ]; then
+    new=$(( current + $val ))
+elif [ "$1" = "-dec" ]; then
+    new=$(( current - $val ))
+fi
+if [ $new -gt $MAX ]; then
+    new=$MAX
+elif [ $new -lt $MIN ]; then
+    new=$MIN
+fi
+if [ "$3" != "-q" ]; then
+    printf "%.0f%%\n" $(echo "$new/$MAX*100" | bc -l)
+fi
+echo $new > "$file"
