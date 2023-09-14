889280d3a76d15dccbddb2220c14e4d473c2de35
Sean Dugre <sdugre@gmail.com>
2023-09-13 23:38:29 -0400

    fix backlight script

--- pkgs/backlight/default.nix
+++ pkgs/backlight/default.nix
@@ -9,23 +9,21 @@
     set -e
     file="/sys/class/backlight/intel_backlight/brightness"
     current=$(cat "$file")
-    new="$current"
-    if [ "$2" != "" ]; then
+    new=$current
+    if [[ "$2" != "" ]]; then
         val=$(echo "$2*$MAX/100" | bc)
     fi
-    if [ "$1" = "-inc" ]; then
-        new=$( current + "$val" )
-    elif [ "$1" = "-dec" ]; then
-        new=$( current - "$val" )
+    if [[ "$1" == "-inc" ]]; then
+        new="$((current+val))"
+    elif [[ "$1" == "-dec" ]]; then
+        new="$((current-val))"
     fi
-    if [ "$new" -gt $MAX ]; then
+    if [[ $new -gt $MAX ]]; then
         new=$MAX
-    elif [ "$new" -lt $MIN ]; then
+    elif [[ $new -lt $MIN ]]; then
         new=$MIN
     fi
-    if [ "$3" != "-q" ]; then
-        printf "%.0f%%\n" "$(echo "$new/$MAX*100" | bc -l)"
-    fi
+    printf "%.0f%%\n" "$(echo "$new/$MAX*100" | bc -l)"
     echo "$new" > "$file"
   '';
 }) // {
