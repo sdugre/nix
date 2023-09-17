8a32d1b39077a71ecdde2dff0d913320af53e0c3
Sean Dugre <sdugre@gmail.com>
2023-09-16 21:44:17 -0400

    adjust swayidle

--- home/sdugre/features/desktop/common/wayland-wm/swayidle.nix
+++ home/sdugre/features/desktop/common/wayland-wm/swayidle.nix
@@ -4,31 +4,41 @@ let
   swaylock = "${config.programs.swaylock.package}/bin/swaylock";
   pgrep = "${pkgs.procps}/bin/pgrep";
   pactl = "${pkgs.pulseaudio}/bin/pactl";
+  backlight = "${pkgs.backlight}/bin/backlight";
 
   isLocked = "${pgrep} -x ${swaylock}";
-  lockTime = 4 * 60; # TODO: configurable desktop (10 min)/laptop (4 min)
+  lockTime = 1 * 60; # TODO: configurable desktop (10 min)/laptop (4 min)
+  dimBeforeTime = 10; # seconds before lockTime to dim screen
+  suspendAfterTime = 1 * 60; # minuts after lockTime to suspend 
 
-  # Makes two timeouts: one for when the screen is not locked (lockTime+timeout) and one for when it is.
-  afterLockTimeout = { timeout, command, resumeCommand ? null }: [
-    { timeout = lockTime + timeout; inherit command resumeCommand; }
-    { command = "${isLocked} && ${command}"; inherit resumeCommand timeout; }
-  ];
 in
 {
   services.swayidle = {
     enable = true;
     systemdTarget = "graphical-session.target";
-    timeouts =
+    timeouts = [
+      # Dim screen
+      {
+        timeout = lockTime - dimBeforeTime;
+        command = "${backlight} -dec 20";
+        resumeCommnad = "${backlight} -inc 20";
+      } 
       # Lock screen
-      [{
+      {
         timeout = lockTime;
         command = "${swaylock} -i ${config.wallpaper} --daemonize";
-      }] ++
-      # Mute mic
-      (afterLockTimeout {
-        timeout = 10;
+      }
+      # Mute Mic
+      {
+        timeout = lockTime + 10;
         command = "${pactl} set-source-mute @DEFAULT_SOURCE@ yes";
-        resumeCommand = "${pactl} set-source-mute @DEFAULT_SOURCE@ no";
-      });
+        resumeCommand = "${pactl} set-source-mute @DEFAULT_SOURCE@ no";        
+      }
+      # Suspend
+      {
+        timeout = lockTime + suspendAfterTime;
+        command = "systemctl suspend";
+      }
+    ]
   };
 }
