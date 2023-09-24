a651efcb16238b09b954136a6434b1fbc173140e
Sean Dugre <sdugre@gmail.com>
2023-09-24 19:30:34 -0400

    tweak rofi shortcut in hyprland

--- home/sdugre/features/desktop/common/wayland-wm/rofi.nix
+++ home/sdugre/features/desktop/common/wayland-wm/rofi.nix
@@ -4,12 +4,17 @@
     enable = true;
     package = pkgs.rofi-wayland;
     extraConfig = {
-      modi = "drun,run";
+      modi = "run,drun,window";
       show-icons = true;
       drun-display-format = "{icon} {name}";
       location = 0;
       disable-history = false;
       hide-scrollbar = false;
+      display-drun = "   Apps ";
+      display-run = "   Run ";
+      display-window = " 﩯  Window";
+      display-Network = " 󰤨  Network";
+      sidebar-mode = true;
     };
   };
 }
