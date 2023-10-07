2a9bfa23460d2b5d4b2bcaa93fe27b8b499e3677
Sean Dugre <sdugre@gmail.com>
2023-10-06 21:11:38 -0400

    go back to unstable

--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
 
   inputs = {
     # Nixpkgs
-#    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";   
-    nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";   
+#    nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
 
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
