5777f76698e5339ce66b0d8f50f5ef3ccfde40cb
Sean Dugre <sdugre@gmail.com>
2023-10-07 23:06:29 -0400

    ?

--- home/sdugre/features/cli/default.nix
+++ home/sdugre/features/cli/default.nix
@@ -11,4 +11,12 @@
     curl
     killall
   ];  
+
+#  home.file = {
+#    nanorc = {
+#      text = "include /run/current-system/sw/share/nano/";
+#      target = ".config/nano/.nanorc";
+#    };
+#  };
+
 }
