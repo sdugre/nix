b855e7f3f11043ff54bf2eb1896636281868586a
Sean <sdugre@gmail.com>
2023-10-29 10:41:17 -0400

    refactor to make vscodium optional. remove from chromebook for space

--- home/sdugre/thinkpad.nix
+++ home/sdugre/thinkpad.nix
@@ -2,8 +2,11 @@
 {
   imports = [ 
     ./global
+
 #    ./features/desktop/gnome
     ./features/desktop/hyprland
+
+    ./features/desktop/common/optional/vscodium.nix
   ];
 
   wallpaper = "~/Documents/nix-config/home/sdugre/wallpapers/landscape-morning.jpg";
