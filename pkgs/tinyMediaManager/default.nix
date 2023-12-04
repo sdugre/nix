12a180d18049dc735ec7cd09927f122d19448e20
Sean Dugre <sdugre@gmail.com>
2023-12-04 15:55:01 -0500

    package tinyMediaManager

---
+++ pkgs/tinyMediaManager/default.nix
@@ -0,0 +1,23 @@
+{ stdenv, lib, fetchurl }:
+
+stdenv.mkDerivation rec {
+  pname = "tinyMediaManager";
+  version = "4.3.14";
+
+  src = fetchurl {
+    url = "https://release.tinymediamanager.org/v4/dist/tmm_${version}_linux-amd64.tar.gz";
+    hash = "";
+  };
+
+  sourceRoot = ".";
+
+  installPhase = ''
+    install -m755 -D ./${pname}/${pname} $out/bin/${pname}
+  '';
+
+  meta = with lib; {
+    homepage = "https://www.tinymediamanager.org";
+    description = "A multi-OS media management tool";
+    platforms = platforms.linux;
+  };
+}
