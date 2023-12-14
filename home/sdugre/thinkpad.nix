0b7ef3f29937de4653e18e7cb57b39f195d863a1
sdugre <sdugre@gmail.com>
2023-12-14 04:18:14 +0000

    initial commit on chummie

--- home/sdugre/thinkpad.nix
+++ home/sdugre/thinkpad.nix
@@ -9,6 +9,9 @@
     ./features/desktop/common/optional/vscodium.nix
   ];
 
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "23.05";
+
   wallpaper = "~/Documents/nix-config/home/sdugre/wallpapers/landscape-morning.jpg";
   colorscheme = inputs.nix-colors.colorSchemes.atlas;
 }
