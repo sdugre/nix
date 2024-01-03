e0ecfc7874378ce2891026f1ed160fa1e5722caa
Sean Dugre <sdugre@gmail.com>
2024-01-02 23:40:14 -0500

    update inputs

--- hosts/common/desktops/hyprland/default.nix
+++ hosts/common/desktops/hyprland/default.nix
@@ -8,23 +8,23 @@
     package = inputs.hyprland.packages.${pkgs.system}.hyprland;	
   };
   environment.systemPackages = with pkgs; [
-    gnome.seahorse
-    gnome-connections
+#    gnome.seahorse
+#    gnome-connections
     gnome.nautilus
   ];
 
-  services.gvfs.enable = true;
+#  services.gvfs.enable = true;
 
-  xdg.portal = {
-    enable = true;
-    wlr.enable = true;
-    config.common.default = "*";
-  };
+#  xdg.portal = {
+#    enable = true;
+#    wlr.enable = true;
+#    config.common.default = "*";
+#  };
 
   # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-  };
+#  services.xserver = {
+#    layout = "us";
+#    xkbVariant = "";
+#  };
 
 }
