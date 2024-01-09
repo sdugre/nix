a43803053a95a541004c183b3f6c0fc460bd0d49
Sean Dugre <sdugre@gmail.com>
2024-01-08 22:58:06 -0500

    testing

--- pkgs/rofi-logout/default.nix
+++ pkgs/rofi-logout/default.nix
@@ -1,7 +1,8 @@
 # logout / suspend / reboot / shutdown script for rofi
 # ${config.programs.swaylock.package}/bin/swaylock -i ${config.wallpaper} --daemonize
 #      swaylock -i ${config.wallpaper} --daemonize
-
+# swaylock --color 0000ff --daemonize
+# let wallpaper = config.home-manager.users.sdugre.wallpaper;
 { pkgs, lib, writeShellApplication, rofi, swaylock, config, ... }: 
 
 (writeShellApplication {
