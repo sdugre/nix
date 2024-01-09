9624a8a05e36ea59981b0f70d2236228a3d1d284
Sean Dugre <sdugre@gmail.com>
2024-01-08 22:59:16 -0500

    testing

--- flake.nix
+++ flake.nix
@@ -43,10 +43,7 @@
       lib =  nixpkgs.lib // home-manager.lib;
       forAllSystems = nixpkgs.lib.genAttrs [
         "aarch64-linux"
-        "i686-linux"
         "x86_64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
       ];
       libx = import ./lib { inherit self inputs outputs; };  
     in
