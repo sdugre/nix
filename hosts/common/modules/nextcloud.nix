87e060fd8fe9f142285888151a86d5b2e0f5fe03
Sean Dugre <sdugre@gmail.com>
2024-02-12 22:20:32 -0500

    nextcloud

--- hosts/common/modules/nextcloud.nix
+++ hosts/common/modules/nextcloud.nix
@@ -1,56 +1,62 @@
 { self, config, lib, pkgs, ... }: {
 
-  services = {
-    nginx.virtualHosts = {
-      "YOUR.DOMAIN.NAME" = {
-        forceSSL = true;
-        enableACME = true;
-        # Use DNS Challenege.
-        acmeRoot = null;
-      };
-    };
+
+  sops.secrets."nextcloud/admin_password" = {
+    owner = "nextcloud";
+    group = "nextcloud";
+  };
+  sops.secrets."nextcloud/db_password" = {
+    owner = "nextcloud";
+    group = "nextcloud";
+  };
     # 
-    nextcloud = {
-      enable = true;
-      hostName = "YOUR.DOMAIN.NAME";
-      # Need to manually increment with every major upgrade.
-      package = pkgs.nextcloud28;
-      # Let NixOS install and configure the database automatically.
-      database.createLocally = true;
-      # Let NixOS install and configure Redis caching automatically.
-      configureRedis = true;
-      # Increase the maximum file upload size.
-      maxUploadSize = "16G";
-      https = true;
-      autoUpdateApps.enable = true;
-      extraAppsEnable = true;
-      extraApps = with config.services.nextcloud.package.packages.apps; {
-        # List of apps we want to install and are already packaged in
-        # https://github.com/NixOS/nixpkgs/blob/master/pkgs/servers/nextcloud/packages/nextcloud-apps.json
-        inherit calendar contacts notes onlyoffice tasks cookbook qownnotesapi;
-        # Custom app example.
-        socialsharing_telegram = pkgs.fetchNextcloudApp rec {
-          url =
-            "https://github.com/nextcloud-releases/socialsharing/releases/download/v3.0.1/socialsharing_telegram-v3.0.1.tar.gz";
-          license = "agpl3";
-          sha256 = "sha256-8XyOslMmzxmX2QsVzYzIJKNw6rVWJ7uDhU1jaKJ0Q8k=";
-        };
-      };
-      config = {
-        overwriteProtocol = "https";
-        defaultPhoneRegion = "US";
-        dbtype = "pgsql";
-        adminuser = "admin";
-        adminpassFile = "/REPLACE/WITH/YOUR/PATH";
-      };
-      # Suggested by Nextcloud's health check.
-      phpOptions."opcache.interned_strings_buffer" = "16";
+  services.nextcloud = {
+    enable = true;
+    hostName = "cloud.seandugre.com";
+    # Need to manually increment with every major upgrade.
+    package = pkgs.nextcloud28;
+    # Let NixOS install and configure the database automatically.
+    database.createLocally = true;
+    # Let NixOS install and configure Redis caching automatically.
+    configureRedis = true;
+    # Increase the maximum file upload size.
+    maxUploadSize = "16G";
+    https = true;
+    autoUpdateApps.enable = true;
+    extraAppsEnable = true;
+    extraApps = with config.services.nextcloud.package.packages.apps; {
+      # List of apps we want to install and are already packaged in
+      # https://github.com/NixOS/nixpkgs/blob/master/pkgs/servers/nextcloud/packages/nextcloud-apps.json
+      inherit calendar contacts notes onlyoffice tasks cookbook qownnotesapi;
+      # Custom app example.
+#      socialsharing_telegram = pkgs.fetchNextcloudApp rec {
+#        url =
+#          "https://github.com/nextcloud-releases/socialsharing/releases/download/v3.0.1/socialsharing_telegram-v3.0.1.tar.gz";
+#        license = "agpl3";
+#        sha256 = "sha256-8XyOslMmzxmX2QsVzYzIJKNw6rVWJ7uDhU1jaKJ0Q8k=";
+#     };
     };
+    config = {
+      overwriteProtocol = "https";
+      defaultPhoneRegion = "US";
+      dbtype = "pgsql";
+      adminuser = "admin";
+      adminpassFile = config.sops.secrets."nextcloud/admin_password".path;
+      dbpassFile = config.sops.secrets."nextcloud/db_password".path;
+    };
+    # Suggested by Nextcloud's health check.
+    phpOptions."opcache.interned_strings_buffer" = "16";
+  };
     # Nightly database backups.
-    postgresqlBackup = {
-      enable = true;
-      startAt = "*-*-* 01:15:00";
-    };
+  services.postgresqlBackup = {
+    enable = true;
+    startAt = "*-*-* 01:15:00";
+  };
+ 
+  environment.persistence = lib.mkIf config.services.persistence.enable {
+    "/persist".directories = [ 
+      "/var/lib/nextcloud" 
+    ];
   };
 
 }
--- hosts/common/modules/nginx/proxy-confs/cloud.seandugre.nix
+++ hosts/common/modules/nginx/proxy-confs/cloud.seandugre.nix
@@ -5,13 +5,14 @@
     enableAuthelia = false;
     extraConfig = ''
     '';
-    locations."/" = {
-      proxyPass = "https://192.168.58:4043";
-      proxyWebsockets = true;
-      extraConfig = ''
-        resolver 127.0.0.11 valid=30s;
-        proxy_max_temp_file_size 2048m;
-      '';
-    };
+# the rest handled by the module
+    # locations."/" = {
+    #   proxyPass = "https://192.168.58:4043";
+    #   proxyWebsockets = true;
+    #   extraConfig = ''
+    #     resolver 127.0.0.11 valid=30s;
+    #     proxy_max_temp_file_size 2048m;
+    #   '';
+    # };
   };
 }
