d0df68780d3c4f7e3df796bd595d2856032477f4
sdugre <sdugre@gmail.com>
2024-08-08 13:21:00 -0400

    fix jackett proxy conf

--- hosts/common/modules/nginx/proxy-confs/jackett.seandugre.nix
+++ hosts/common/modules/nginx/proxy-confs/jackett.seandugre.nix
@@ -6,7 +6,7 @@
     extraConfig = ''
     '';
     locations."/" = {
-      proxyPass = "http://192.168.1.58:9117";
+      proxyPass = "http://192.168.1.200:9118";
       proxyWebsockets = true;
       extraConfig = ''
         resolver 127.0.0.11 valid=30s;
