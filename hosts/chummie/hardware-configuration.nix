489f4a78c0deccff6d2ecb627d75d20c2fb26262
sdugre <sdugre@gmail.com>
2024-10-10 22:19:33 -0400

    clean up network drives from NAS

--- hosts/chummie/hardware-configuration.nix
+++ hosts/chummie/hardware-configuration.nix
@@ -58,11 +58,6 @@
       fsType = "zfs";
     };
 
-  fileSystems."/mnt/x" =
-    { device = "tank/x";
-      fsType = "zfs";
-    };
-
   swapDevices =
     [ { device = "/dev/disk/by-uuid/b2e52443-a32a-46c4-ad5e-2585659cf8ee"; }
     ];
--- hosts/common/optional/networkDrives.nix
+++ hosts/common/optional/networkDrives.nix
@@ -9,41 +9,41 @@
   in
 {
   # mount NAS drives via tailscale
-  fileSystems."/mnt/video" = {
-    device = nasIP + ":/volume1/video";
-    fsType = "nfs";
-    options = mountOptions;
-  };
-  fileSystems."/mnt/music" = {
-    device = nasIP + ":/volume1/music";
-    fsType = "nfs";
-    options = mountOptions;
-  }; 
-  fileSystems."/mnt/photo" = {
-    device = nasIP + ":/volume1/photo";
-    fsType = "nfs";
-    options = mountOptions;
-  }; 
-  fileSystems."/mnt/downloads" = {
-    device = nasIP + ":/volume1/downloads";
-    fsType = "nfs";
-    options = mountOptions;
-  };
+#  fileSystems."/mnt/video" = {
+#    device = nasIP + ":/volume1/video";
+#    fsType = "nfs";
+#    options = mountOptions;
+#  };
+#  fileSystems."/mnt/music" = {
+#    device = nasIP + ":/volume1/music";
+#    fsType = "nfs";
+#    options = mountOptions;
+#  }; 
+#  fileSystems."/mnt/photo" = {
+#    device = nasIP + ":/volume1/photo";
+#    fsType = "nfs";
+#    options = mountOptions;
+#  }; 
+#  fileSystems."/mnt/downloads" = {
+#    device = nasIP + ":/volume1/downloads";
+#    fsType = "nfs";
+#    options = mountOptions;
+#  };
   fileSystems."/mnt/homes" = {
     device = nasIP + ":/volume1/homes";
     fsType = "nfs";
     options = mountOptions;
   }; 
-  fileSystems."/mnt/docs" = {
-    device = nasIP + ":/volume1/docs";
-    fsType = "nfs";
-    options = mountOptions;
-  };
-  fileSystems."/mnt/books" = {
-    device = nasIP + ":/volume1/books";
-    fsType = "nfs";
-    options = mountOptions;
-  };  
+#  fileSystems."/mnt/docs" = {
+#    device = nasIP + ":/volume1/docs";
+#    fsType = "nfs";
+#    options = mountOptions;
+#  };
+#  fileSystems."/mnt/books" = {
+#    device = nasIP + ":/volume1/books";
+#    fsType = "nfs";
+#    options = mountOptions;
+#  };  
   fileSystems."/mnt/podcasts" = {
     device = nasIP + ":/volume1/podcasts";
     fsType = "nfs";
