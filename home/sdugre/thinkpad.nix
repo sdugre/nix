4dc94c697616d383e29f089fa0a06f547e73d2ab
Sean Dugre <sdugre@gmail.com>
2024-10-25 22:07:47 -0400

    remove calibre

--- home/sdugre/thinkpad.nix
+++ home/sdugre/thinkpad.nix
@@ -4,7 +4,7 @@
 
     # Optional machine specific packages
     ./common/software/gui/vscodium.nix
-    ./common/software/gui/calibre.nix
+#    ./common/software/gui/calibre.nix    # use container on chummie instead
   ];
 
   programs.chromium.enable = true;  # for testing, esphome, etc.
