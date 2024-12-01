3c286f72a6b158cb337738f89fddbed9c2c7bc35
sdugre <sdugre@gmail.com>
2024-12-01 11:05:44 -0500

    fix cars proxy conf

--- hosts/common/modules/nginx/proxy-confs/cars.seandugre.nix
+++ hosts/common/modules/nginx/proxy-confs/cars.seandugre.nix
@@ -10,15 +10,6 @@
       proxyWebsockets = true;
       extraConfig = ''
         resolver 127.0.0.11 valid=30s;
-	client_max_body_size               50000M;
-        proxy_set_header Host              $http_host;
-        proxy_set_header X-Real-IP         $remote_addr;
-        proxy_set_header X-Forwarded-For   $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
-        proxy_http_version 1.1;
-        proxy_set_header   Upgrade    $http_upgrade;
-        proxy_set_header   Connection "upgrade";
-        proxy_redirect off;
       '';
     };
   };
