8f62fb5847b097e5e12fd6389b0ed3ec2f77e8c8
Sean Dugre <sdugre@gmail.com>
2025-03-15 21:09:20 -0400

    feat:stylix

--- home/sdugre/common/software/gui/mako.nix
+++ home/sdugre/common/software/gui/mako.nix
@@ -13,7 +13,7 @@ in {
     #        "${config.gtk.iconTheme.package}/share/icons/Papirus-Dark"
     #      else
     #        "${config.gtk.iconTheme.package}/share/icons/Papirus-Light";
-    font = "${config.fontProfiles.regular.family} 12";
+#    font = "${config.fontProfiles.regular.family} 12";
     padding = "10,20";
     anchor = "top-center";
     width = 400;
--- home/sdugre/thinkpad.nix
+++ home/sdugre/thinkpad.nix
@@ -11,8 +11,20 @@
   imports = [
     # Optional machine specific packages
     ./common/software/gui/vscodium.nix
+    inputs.stylix.homeManagerModules.stylix
   ];
 
   wallpaper = wallpaperPath;
   colorscheme = inputs.nix-colors.colorSchemes.atlas;
+  stylix.enable = true;
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+#  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/atlas.yaml";
+#  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/dracula.yaml";
+  stylix.image = ./landscape-morning.jpg;
+  stylix.targets.hyprlock.enable = false;
+  stylix.targets.mako.enable = false;
+#  stylix.targets.waybar.enableLeftBackColors = true;
+#  stylix.targets.waybar.enableCenterBackColors = true;
+#  stylix.targets.waybar.enableRightBackColors = true;
+  stylix.polarity = "dark";
 }
