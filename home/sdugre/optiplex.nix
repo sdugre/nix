1b88746556e842119991722b7c77c5fc864e35ca
sdugre <sdugre@gmail.com>
2025-03-20 18:13:39 -0400

    fix(hyprlock/mako): move colors to stylix

--- home/sdugre/optiplex.nix
+++ home/sdugre/optiplex.nix
@@ -18,14 +18,10 @@
   ];
 
   wallpaper = wallpaperPath;
-#  colorscheme = inputs.nix-colors.colorSchemes.atlas;
   stylix.enable = true;
 #  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
 #  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/atlas.yaml";
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/dracula.yaml";
-#  stylix.image = builtins.toPath wallpaperPath;
-  stylix.targets.hyprlock.enable = false;
-  stylix.targets.mako.enable = false;
   stylix.polarity = "dark";
   stylix.targets.vscode.profileNames = [ "Default" ];
   stylix.targets.firefox.profileNames = [ "default" ];
