b32d6dd3acacc82e69061cc30cb9e31137c149a0
Sean Dugre <sdugre@gmail.com>
2025-03-19 22:23:07 -0400

    chore(stylix/nix-colors): refactor, change to stylix

--- home/sdugre/thinkpad.nix
+++ home/sdugre/thinkpad.nix
@@ -2,7 +2,6 @@
   inputs,
   pkgs,
   config,
-  nix-colors,
   ...
 }: 
   let 
@@ -15,21 +14,16 @@
   ];
   device.isLaptop = true;
   wallpaper = wallpaperPath;
-  colorscheme = inputs.nix-colors.colorSchemes.atlas;
   stylix.enable = true;
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
 #  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/atlas.yaml";
 #  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/dracula.yaml";
-#  stylix.image = ./landscape-morning.jpg;
-  stylix.targets.hyprlock.enable = false;
-  stylix.targets.mako.enable = false;
   stylix.polarity = "dark";
   stylix.targets.vscode.profileNames = [ "Default" ];
   stylix.targets.firefox.profileNames = [ "default" ];
   stylix.cursor.package = pkgs.bibata-cursors;
   stylix.cursor.name = "Bibata-Modern-Classic";
   stylix.cursor.size = 20;
-
 
   monitors = [
     {
