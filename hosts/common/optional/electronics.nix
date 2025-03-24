100fd6e0800254cb3cd339fe30e497ee8b9c6cfe
sdugre <sdugre@gmail.com>
2025-03-23 22:03:00 -0400

    feat: add kiCad

--- hosts/common/optional/electronics.nix
+++ hosts/common/optional/electronics.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
-  environment.systemPackages = [
-    #    pkgs.kicad  temp disable  -failing update due to wxpython
-    pkgs.fritzing
-    pkgs.ngspice
+  environment.systemPackages = with pkgs; [
+    kicad
+    fritzing
+    ngspice
   ];
 }
