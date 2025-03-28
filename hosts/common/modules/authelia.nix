8056d60d1b99b7a1ab7cbf39acbb777e40b88fbf
sdugre <sdugre@gmail.com>
2025-03-28 17:39:41 -0400

    feat: change authentication backend to LDAP

--- hosts/common/modules/authelia.nix
+++ hosts/common/modules/authelia.nix
@@ -5,52 +5,25 @@
   hostname,
   ...
 }: {
-  sops.secrets."authelia/jwtSecretFile" = {
-    sopsFile = ../../${hostname}/secrets.yaml;
-    mode = "0400";
-    owner = "authelia-main";
-    group = "authelia-main";
-    restartUnits = ["authelia-main.service"];
-  };
-
-  sops.secrets."authelia/storageEncryptionKeyFile" = {
-    sopsFile = ../../${hostname}/secrets.yaml;
-    mode = "0400";
-    owner = "authelia-main";
-    group = "authelia-main";
-    restartUnits = ["authelia-main.service"];
-  };
-
-  sops.secrets."authelia/sessionSecretFile" = {
-    sopsFile = ../../${hostname}/secrets.yaml;
-    mode = "0400";
-    owner = "authelia-main";
-    group = "authelia-main";
-    restartUnits = ["authelia-main.service"];
-  };
-
-  sops.secrets."authelia_login" = {
-    sopsFile = ../../${hostname}/secrets.yaml;
-    mode = "0400";
-    owner = "authelia-main";
-    group = "authelia-main";
-    restartUnits = ["authelia-main.service"];
-  };
-
-  sops.secrets."authelia/oidcHmacSecretFile" = {
-    sopsFile = ../../${hostname}/secrets.yaml;
-    mode = "0400";
-    owner = "authelia-main";
-    group = "authelia-main";
-    restartUnits = ["authelia-main.service"];
-  };
-
-  sops.secrets."authelia/oidcIssuerPrivateKeyFile" = {
-    sopsFile = ../../${hostname}/secrets.yaml;
-    mode = "0400";
-    owner = "authelia-main";
-    group = "authelia-main";
-    restartUnits = ["authelia-main.service"];
+  sops = {
+    secrets = let
+      sopsFile = ../../${hostname}/secrets.yaml;
+      mode = "0400";
+      owner = "authelia-main";
+      group = "authelia-main";
+      restartUnits = ["authelia-main.service"];
+      cfg = {
+        inherit owner group mode restartUnits sopsFile;
+      };
+    in {
+      "authelia/jwtSecretFile" = cfg; 
+      "authelia/storageEncryptionKeyFile" = cfg;
+      "authelia/sessionSecretFile" = cfg;
+      "authelia_login" = cfg;
+      "authelia/oidcHmacSecretFile" = cfg;
+      "authelia/oidcIssuerPrivateKeyFile" = cfg;
+      "authelia/lldap" = cfg;
+    };
   };
 
   environment.persistence = lib.mkIf config.services.persistence.enable {
@@ -68,7 +41,9 @@
       oidcIssuerPrivateKeyFile = config.sops.secrets."authelia/oidcIssuerPrivateKeyFile".path;
       oidcHmacSecretFile = config.sops.secrets."authelia/oidcHmacSecretFile".path;
     };
-
+    environmentVariables = {
+      AUTHELIA_AUTHENTICATION_BACKEND_LDAP_PASSWORD_FILE = config.sops.secrets."authelia/lldap".path;
+    };
     settings = {
       theme = "dark";
       default_redirection_url = "https://seandugre.com";
@@ -84,52 +59,52 @@
         format = "text";
       };
 
-      authentication_backend = {
-        file = {
-          path = config.sops.secrets."authelia_login".path;
-          watch = false;
-          search = {
-            email = false;
-            case_insensitive = false;
-          };
-          password = {
-            algorithm = "argon2";
-            argon2 = {
-              variant = "argon2id";
-              iterations = 3;
-              memory = 65536;
-              parallelism = 4;
-              key_length = 32;
-              salt_length = 16;
-            };
-          };
-        };
-      };
-
-# DRAFT #
 #      authentication_backend = {
-#        password_reset.disable = false;
-#        refresh_interval = "1m";
-#        ldap = {
-#          implementation = "custom";
-#          address = "ldap://127.0.0.1:${toString config.services.lldap.settings.ldap_port}";
-#          timeout = "5m";
-#          start_tls = false;
-#          base_dn = "dc=seandugre,dc=com";
-#          additional_users_dn = "ou=people";
-#          users_filter = "(&({username_attribute}={input})(objectClass=person))";
-#          additional_groups_dn = "ou=groups";
-#          groups_filter = "(member={dn})";
-#          attributes = {
-#            user_name = "uid";
-#            member_of = "memberOf";
-#            group_name = "cn";
-#            mail = "mail";
-#            display_name_attribute = "displayName";
+#        file = {
+#          path = config.sops.secrets."authelia_login".path;
+#          watch = false;
+#          search = {
+#            email = false;
+#            case_insensitive = false;
 #          };
-#          user = "uid=admin,ou=people,dc=seandugre,dc=com";
+#          password = {
+#            algorithm = "argon2";
+#            argon2 = {
+#              variant = "argon2id";
+#              iterations = 3;
+#              memory = 65536;
+#              parallelism = 4;
+#              key_length = 32;
+#              salt_length = 16;
+#            };
+#          };
 #        };
 #      };
+
+# DRAFT #
+      authentication_backend = {
+        password_reset.disable = false;
+        refresh_interval = "1m";
+        ldap = {
+          implementation = "custom";
+          address = "ldap://127.0.0.1:${toString config.services.lldap.settings.ldap_port}";
+          timeout = "5m";
+          start_tls = false;
+          base_dn = "dc=seandugre,dc=com";
+          additional_users_dn = "ou=people";
+          users_filter = "(&({username_attribute}={input})(objectClass=person))";
+          additional_groups_dn = "ou=groups";
+          groups_filter = "(member={dn})";
+          attributes = {
+            username = "uid";
+            member_of = "memberOf";
+            group_name = "cn";
+            mail = "mail";
+            display_name = "displayName";
+          };
+          user = "uid=admin,ou=people,dc=seandugre,dc=com";
+        };
+      };
 
       identity_providers.oidc = {
         cors = {
