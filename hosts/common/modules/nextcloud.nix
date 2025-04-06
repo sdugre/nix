512f3c89591f6dc1e505bf62909aba3cd03d9c3d
sdugre <sdugre@gmail.com>
2025-04-06 15:00:08 -0400

    feat: add collabora to nextcloud; bump lock

--- hosts/common/modules/nextcloud.nix
+++ hosts/common/modules/nextcloud.nix
@@ -33,7 +33,7 @@
       # List of apps we want to install and are already packaged in
       # https://github.com/NixOS/nixpkgs/blob/master/pkgs/servers/nextcloud/packages/nextcloud-apps.json
       #      inherit calendar contacts notes onlyoffice tasks cookbook qownnotesapi;
-      inherit calendar contacts tasks onlyoffice;
+      inherit calendar contacts tasks richdocuments;
       # Custom app example.
       #      socialsharing_telegram = pkgs.fetchNextcloudApp rec {
       #        url =
@@ -62,33 +62,63 @@
 
 #  WAITING FOR https://github.com/NixOS/nixpkgs/issues/383483 TO BE RESOLVED
 #  REF:  https://diogotc.com/blog/collabora-nextcloud-nixos/
-#  services.collabora-online = {
-#    enable = true;
-#    port = 9980; # default
-#    settings = {
-#      # Rely on reverse proxy for SSL
-#      ssl = {
-#        enable = false;
-#        termination = true;
-#      };
-#
-#      # Listen on loopback interface only, and accept requests from ::1
-#      net = {
-#        listen = "loopback";
-#        post_allow.host = ["::1"];
-#      };
-#
-#      # Restrict loading documents from WOPI Host nextcloud.example.com
-#      storage.wopi = {
-#        "@allow" = true;
-#        host = ["cloud.seandugre.com"];
-#      };
-#
-#      # Set FQDN of server
-#      server_name = "office.seandugre.com";
-#    };
-#  };
+  services.collabora-online = {
+    enable = true;
+    port = 9980; # default
+    settings = {
+      # Rely on reverse proxy for SSL
+      ssl = {
+        enable = false;
+        termination = true;
+      };
+
+      # Listen on loopback interface only, and accept requests from ::1
+      net = {
+        listen = "loopback";
+        post_allow.host = ["::1"];
+      };
+
+      # Restrict loading documents from WOPI Host nextcloud.example.com
+      storage.wopi = {
+        "@allow" = true;
+        host = ["cloud.seandugre.com"];
+      };
+
+      # Set FQDN of server
+      server_name = "office.seandugre.com";
+    };
+  };
   
+  systemd.services.nextcloud-config-collabora = let
+    inherit (config.services.nextcloud) occ;
+
+    wopi_url = "http://[::1]:${toString config.services.collabora-online.port}";
+    public_wopi_url = "https://office.seandugre.com";
+    wopi_allowlist = lib.concatStringsSep "," [
+      "127.0.0.1"
+      "::1"
+    ];
+  in {
+    wantedBy = ["multi-user.target"];
+    after = ["nextcloud-setup.service" "coolwsd.service"];
+    requires = ["coolwsd.service"];
+    script = ''
+      ${occ}/bin/nextcloud-occ config:app:set richdocuments wopi_url --value ${lib.escapeShellArg wopi_url}
+      ${occ}/bin/nextcloud-occ config:app:set richdocuments public_wopi_url --value ${lib.escapeShellArg public_wopi_url}
+      ${occ}/bin/nextcloud-occ config:app:set richdocuments wopi_allowlist --value ${lib.escapeShellArg wopi_allowlist}
+      ${occ}/bin/nextcloud-occ richdocuments:setup
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      User = "nextcloud";
+    };
+  };
+
+  networking.hosts = {
+    "127.0.0.1" = ["cloud.seandugre.com" "office.seandugre.com"];
+    "::1" = ["cloud.seandugre.com" "office.seandugre.com"];
+  };
+
   services.nginx.virtualHosts."cloud.seandugre.com" = {
     useACMEHost = "seandugre.com";
     forceSSL = true;
@@ -106,14 +136,14 @@
     # };
   };
     
-#  services.nginx.virtualHosts."office.seandugre.com" =  {
-#    enableACME = true;
-#    forceSSL = true;
-#    locations."/" = {
-#      proxyPass = "http://[::1]:${toString config.services.collabora-online.port}";
-#      proxyWebsockets = true; # collabora uses websockets
-#    };
-#  };
+  services.nginx.virtualHosts."office.seandugre.com" =  {
+    enableACME = true;
+    forceSSL = true;
+    locations."/" = {
+      proxyPass = "http://[::1]:${toString config.services.collabora-online.port}";
+      proxyWebsockets = true; # collabora uses websockets
+    };
+  };
 
   environment.persistence = lib.mkIf config.services.persistence.enable {
     "/persist".directories = [
