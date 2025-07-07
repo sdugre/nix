b5f9ae242da4269c52727f63d9bc551c065795a9
sdugre <sdugre@gmail.com>
2025-07-06 23:02:15 -0400

    feat: refine script

--- pkgs/nextcloud-backup-helper/default.nix
+++ pkgs/nextcloud-backup-helper/default.nix
@@ -7,7 +7,7 @@
 
 (writeShellApplication {
   name = "nextcloud-backup-helper";
-  runtimeInputs = [ pkgs.rsync ];
+  runtimeInputs = [ pkgs.rsync pkgs.findutils ];
   text = 
     /*
     bash
@@ -17,10 +17,27 @@
       
       SRC="/mnt/photos/!backup-phone-sean"
       TARGET="/mnt/photos/_ready-for-review"
+      TARGET_VID="/mnt/photos/_ready-for-review/vid"
+
       mkdir -p "$TARGET"
-      rsync -rt --remove-source-files "$SRC"/ "$TARGET"/
-      chmod -R a=,a+rX,u+w,g+w "$TARGET" 
-      chown -R sdugre:photos "$TARGET"
+      mkdir -p "$TARGET_VID"
+
+      # Move video files to VIDEO_DIR
+      find "$SRC" -type f \( \
+        -iname '*.mp4' -o -iname '*.mov' \
+        -o -iname '*.mpeg' -o -iname '*.mpg' \
+        \) -exec mv -v {} "$TARGET_VID" \;
+
+      rsync -rt --remove-source-files \
+        --exclude='*.mp4' --exclude='*.mov' \
+        --exclude='*.mpeg' --exclude='*.mpg' \
+        "$SRC"/ "$TARGET"/
+
+      # Delete empty directories from SRC
+      find "$SRC" -mindepth 1 -type d -empty -delete
+
+      chmod -R a=,a+rX,u+w,g+w "$TARGET" "$TARGET_VID" 
+      chown -R sdugre:photos "$TARGET" "$TARGET_VID"
     '';
 })
 // {
