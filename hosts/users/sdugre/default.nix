e568ec1c570413005374630089be5403ff98ba08
sdugre <sdugre@gmail.com>
2025-10-24 22:29:32 -0400

    chore: add macbook to auth keys

--- hosts/users/sdugre/default.nix
+++ hosts/users/sdugre/default.nix
@@ -29,6 +29,7 @@ in {
     hashedPassword = "$6$VFLMhcigCBrLJ3PF$I7jBNN15btQBG48dd.s987gZKYjkx2Ku796TmMSKV4bhn7p/sEF9F7E0MygJTCpfKuAEfcdxBeJ7ZFz0c/OxG0";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP08ryfgQQWLbhbNYqwEBTKCBIArQPalcjtRo54mpr/v sdugre@gmail.com" # thinkpad
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHMKJfo0RNyT4J8Xdag6sfCSGgdKobIE1EeOEgPWi69a sdugre@macbookPro" # macbookPro
     ];
   };
 
