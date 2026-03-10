0f58ca3bf909e9939525269fbdb39c116f336475
Sean Dugre <sdugre@gmail.com>
2026-03-09 22:33:23 -0400

    feat(hyprland): add scrolling and monocle workspaces and binds

--- home/sdugre/common/desktops/hyprland/default.nix
+++ home/sdugre/common/desktops/hyprland/default.nix
@@ -165,12 +165,17 @@
         "SUPER, 2, workspace, 2"
         "SUPER, 3, workspace, 3"
         "SUPER, 4, workspace, 4"
+        "SUPER, 8, workspace, 8"
+        "SUPER, 9, workspace, 9"
         "SUPER, Tab, workspace, e+1"
+        "SUPER SHIFT, Tab, workspace, e-1"
         # Move active window to a workspace with mainMod + SHIFT + [0-9]
         "SUPER SHIFT, 1, movetoworkspace, 1"
         "SUPER SHIFT, 2, movetoworkspace, 2"
         "SUPER SHIFT, 3, movetoworkspace, 3"
         "SUPER SHIFT, 4, movetoworkspace, 4"
+        "SUPER SHIFT, 8, movetoworkspace, 8"
+        "SUPER SHIFT, 9, movetoworkspace, 9"
         # Scroll through existing workspaces with mainMod + scroll
         "SUPER, mouse_down, workspace, e+1"
         "SUPER, mouse_up, workspace, e-1"
@@ -183,6 +188,21 @@
         "CONTROL, Print, exec, ${grimblast} --notify --freeze copy screen"
         "SUPER, Print, exec, ${grimblast} --notify --freeze copy area"
         "ALT, Print, exec, ${grimblast} --notify --freeze copy area"
+        # Scrolling Mode
+        # Move the view left/right by columns
+        "SUPER, bracketleft,  layoutmsg, move -col"
+        "SUPER, bracketright, layoutmsg, move +col"
+        # Move focus between columns and wrap
+        "SUPER, h, layoutmsg, focus l"
+        "SUPER, l, layoutmsg, focus r"
+        # Swap columns
+        "SUPER SHIFT, h, layoutmsg, swapcol l"
+        "SUPER SHIFT, l, layoutmsg, swapcol r"
+        # Resize current column (wider/narrower)
+        "SUPER, minus, layoutmsg, colresize -0.1"
+        "SUPER, equal, layoutmsg, colresize +0.1"
+        # Monocle Mode
+        "ALT, TAB, layoutmsg, cyclenext "
       ];
 
       bindm = [
@@ -214,7 +234,8 @@
 
       workspace = map (m: "name:${m.workspace},monitor:${m.name}") (
         lib.filter (m: m.enabled && m.workspace != null) (config.monitors)
-      );
+      ) ++ [ "9,layout:scrolling" ]
+        ++ [ "8,layout:monocle" ];
     };
   };
 }
