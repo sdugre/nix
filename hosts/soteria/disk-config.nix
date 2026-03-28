b9ff262f1680cf0edcf5d0932a5442541472ef34
Sean Dugre <sdugre@gmail.com>
2026-03-28 18:35:18 -0400

    feat: disko

--- hosts/soteria/disk-config.nix
+++ hosts/soteria/disk-config.nix
@@ -59,7 +59,73 @@
         };
       };
     };
+  
+    # First 6 TB HDD
+    hdd1 = {
+      type = "disk";
+      device = "/dev/sda";
+      content = {
+        type = "gpt";
+        partitions = {
+          zfs = {
+            size = "100%";
+            content = {
+              type = "zfs";
+              pool = "tank";
+            };
+          };
+        };
+      };
+    };
+
+    # Second 6 TB HDD (mirror)
+    hdd2 = {
+      type = "disk";
+      device = "/dev/sdb";
+      content = {
+        type = "gpt";
+        partitions = {
+          zfs = {
+            size = "100%";
+            content = {
+              type = "zfs";
+              pool = "tank";
+            };
+          };
+        };
+      };
+    };
+
+    # ZFS pool definition (mirror)
+    zpool = {
+      tank = {
+        type = "zpool";
+        mode = "mirror";
+        options = {          # corresponds to -o pool property options
+          cachefile = "none";
+          ashift = "12";
+        };
+        rootFsOptions = {    # corresponds to -O file system property options
+          compression = "on";
+          atime = "off";
+          recordsize = "1M"; # general purpose file sharing per Jim Salter
+          xattr = "sa";
+          acltype = "posixacl";
+        };
+        mountpoint = "none";  # datasets handle mounts
+      };
+    };
   };
+
+  # Very basic placeholder mount for the pool itself (optional)
+  # not needed for `disko‑zfs` datasets, but sometimes convenient
+  # fileSystems."/tank" = {
+  #   device = "tank";
+  #   fsType = "zfs";
+  #   options = [ "xattr" "atime" ];
+  #   neededForBoot = false;
+  # };  
+
 
   fileSystems."/persist".neededForBoot = true;
   fileSystems."/var/log".neededForBoot = true;
