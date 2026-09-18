0b44947a56f2f0353585c32e3099e62d0b21be55
sdugre <sdugre@gmail.com>
2026-09-18 17:10:25 -0400

    chore: bump lock

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -14,7 +14,7 @@
     nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
 
     settings = {
-      experimental-features = "nix-command flakes";
+      experimental-features = ["nix-command flakes"];
       auto-optimise-store = true;
       warn-dirty = false;
       substituters = ["https://hyprland.cachix.org"];
